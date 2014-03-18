bd45c8bb08c1c77b3e685c4794830b49aa5e3556
speedyswimmer1000 <speedyswimmer1000@gmail.com>
2014-03-17 21:28:24 -0600

    Param_vars update serial ports

--- param_vars.py
+++ param_vars.py
@@ -6,6 +6,6 @@ motor_min = 0
 motor_max = 1 # These are the two acceptable values for motor enumerated values. 
 e_code = 1 # Error code
 
-port_80 = '/dev/tty1'
-port_81 = '/dev/tty2'
+port_80 = '/dev/ttyPCH1'
+port_81 = '/dev/ttyPCH2'
 baud_rate = 19200
